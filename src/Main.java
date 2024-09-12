d732e290c0b8010486bfb587f14ae61efab91f91
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2024-09-11 21:39:20 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,6 +17,7 @@ public class Main {
         curso2.setCargaHoraria(4);
 
         //Conteudo conteudo = new Curso();
+        //Conteudo conteudo = new Mentoria();
 
         //List<String> palavras = new ArrayList<>();
 
@@ -27,8 +26,41 @@ public class Main {
         mentoria.setDescricao("descricao mentoria");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRodrigo = new Dev();
+        devRodrigo.setNome("Rodrigo");
+        devRodrigo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Rodrigo " + devRodrigo.getConteudosIncritos());
+        devRodrigo.progredir();
+        devRodrigo.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Rodrigo " + devRodrigo.getConteudosIncritos());
+        System.out.println("Conteudos concluidos Rodrigo " + devRodrigo.getConteudosConcluidos());
+        System.out.println("XP: " +devRodrigo.calcularTotalXp());
+
+        System.out.println("-------------");
+
+        Dev devLeid = new Dev();
+        devLeid.setNome("Leid");
+        devLeid.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Leid " + devLeid.getConteudosIncritos());
+        devLeid.progredir();
+        devLeid.progredir();
+        devLeid.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Leid " + devLeid.getConteudosIncritos());
+        System.out.println("Conteudos concluidos Leid " + devLeid.getConteudosConcluidos());
+        System.out.println("XP: " +devLeid.calcularTotalXp());
+
     }
 }
